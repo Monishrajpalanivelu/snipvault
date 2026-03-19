92e45fa29716cf388c1e20286af079f911b95308
Monishrajpalanivelu <monishrajpalanivelu@gmail.com>
2026-03-19 23:22:03 +0530

    Phase 1: Spring Boot setup + Snippet entity

---
+++ src/main/java/com/snippetvault/snipvault/SnipvaultApplication.java
@@ -0,0 +1,13 @@
+package com.snippetvault.snipvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnipvaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnipvaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snippetvault/snipvault/SnipvaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snippetvault.snipvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnipvaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
